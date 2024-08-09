4307dd6cbfc2640e2a9c414b5c00d1e576b2e56f
tnkii-dev <fsan.tanukii@gmail.com>
2024-08-09 12:59:45 -0600

    meme 240809 && carga de contenido por json !js

--- js/loadContent.js
+++ js/loadContent.js
@@ -31,101 +31,84 @@ function loadContent() {
   }
 
   var momos = document.getElementById('momos');
-  const memes = [
-    "240808_1",
-    "240807_2",
-    "240807_1"
-  ]
   while (momos.firstChild) {
     momos.removeChild(momos.firstChild);
   }
-  function loadMemes(index) {
-    if (index < memes.length) {
-      setTimeout(() => {
-        memePath = "momos/" + memes[index] + ".jpg";
-        let link = document.createElement("a");
-        link.href = memePath;
-        link.target = "_blank"
-        let image = document.createElement("img");
-        image.src = memePath;
 
-        link.appendChild(image);
-        momos.appendChild(link);
-        loadMemes(index + 1)
-      }, 10);
-    } else {
+  fetch("js/momos.json")
+  .then(response => response.json())
+  .then(data => {
+    function showMemes(index) {
+      if (index < data.length) {
+        setTimeout(() => {
+          memePath = "momos/" + data[index].id + ".jpg";
+          let link = document.createElement("a");
+          link.href = memePath;
+          link.target = "_blank"
+          let image = document.createElement("img");
+          image.src = memePath;
+  
+          link.appendChild(image);
+          momos.appendChild(link);
+          showMemes(index + 1)
+        }, 10);
+      }
     }
-  }
-  loadMemes(0);
+    showMemes(0);
+  })
 
   var filter = params.get('filter');
   var cajas = document.getElementById('cajas');
-  const contents = [
-    "240722",
-    "240703",
-    "240628",
-    "240622",
-    "240613",
-    "240609",
-    "240511",
-    "240423",
-    "240416",
-    "240414",
-    "240406",
-    "240324",
-    "240323",
-    "240322",
-    "231216",
-    "231203",
-    "231115"
-  ];
   while (cajas.firstChild) {
     cajas.removeChild(cajas.firstChild);
   }
+  fetch("js/entries.json")
+  .then(response => response.json())
+  .then(data => {
+    function loadSequentially(index, adCount) {
+      if (index < data.length) {
+        setTimeout(() => {
+          let entryPath = 'entries/' + lang + '/' + data[index].id + '.json';
+          fetch(entryPath)
+            .then(response => response.json())
+            .then(data => {
+              if (filter == "all" || data.type == filter) {
+                var caja = document.createElement('a');
+                caja.className = 'caja';
+                caja.style = "border: 2px solid" + data.color + ";";
+                caja.href = 'entries/entry.html?id=' + data.id;
+                var h2 = document.createElement('h2');
+                h2.textContent = data.title;
+                h2.style.color = data.color;
+                var h3 = document.createElement('h3');
+                h3.textContent = data.date;
+                h3.style.color = data.color + "aa";
+                var h4 = document.createElement('h4');
+                h4.textContent = data.langs;
+                h4.style.color = data.color;
+                var p = document.createElement('p');
+                p.textContent = data.summary;
+                p.style.color = data.color;
+  
+                caja.appendChild(h2);
+                caja.appendChild(h3);
+                caja.appendChild(h4);
+                caja.appendChild(p);
 
-  function loadSequentially(index) {
-    if (index < contents.length) {
-      setTimeout(() => {
-        entryPath = 'entries/' + lang + '/' + contents[index] + '.json';
-        fetch(entryPath)
-          .then(response => response.json())
-          .then(data => {
-            if (filter == "all" || data.type == filter) {
-              var caja = document.createElement('a');
-              caja.className = 'caja';
-              caja.style = "border: 2px solid" + data.color + ";";
-              caja.href = 'entries/entry.html?id=' + data.id;
-              var h2 = document.createElement('h2');
-              h2.textContent = data.title;
-              h2.style.color = data.color;
-              var h3 = document.createElement('h3');
-              h3.textContent = data.date;
-              h3.style.color = data.color + "aa";
-              var h4 = document.createElement('h4');
-              h4.textContent = data.langs;
-              h4.style.color = data.color;
-              var p = document.createElement('p');
-              p.textContent = data.summary;
-              p.style.color = data.color;
-
-              caja.appendChild(h2);
-              caja.appendChild(h3);
-              caja.appendChild(h4);
-              caja.appendChild(p);
-              cajas.appendChild(caja);
-            }
-            // Llama recursivamente a la función para cargar el siguiente contenido
-            loadSequentially(index + 1);
-          })
-          .catch(error => console.error('Error al cargar el archivo JSON:', error));
-      }, 10); // Retraso de 100 ms entre cada carga
-    } else {
-      loader.style.display = "none";
-      filter = document.getElementById("filter"); filter.style.display = "flex";
-      filterS = document.getElementById("filterS"); filterS.style.display = "none";
-      reload = document.getElementById("reload"); reload.style.display = "flex";
-      reloadS = document.getElementById("reloadS"); reloadS.style.display = "none";
+                cajas.appendChild(caja);
+              }
+              // Llama recursivamente a la función para cargar el siguiente contenido
+              loadSequentially(index + 1);
+            })
+        }, 10); // Retraso de 100 ms entre cada carga
+      } else {
+        loader.style.display = "none";
+        filter = document.getElementById("filter"); filter.style.display = "flex";
+        filterS = document.getElementById("filterS"); filterS.style.display = "none";
+        reload = document.getElementById("reload"); reload.style.display = "flex";
+        reloadS = document.getElementById("reloadS"); reloadS.style.display = "none";
+      }
     }
-  }
-  loadSequentially(0);
+    loadSequentially(0);
+  })
 }
